0a5a89a36f62d7283da34234b302ce20ecdb13f7
Jeff Wendling <jeff@spacemonkey.com>
2017-04-23 15:42:57 -0600

    only support 1.8 but also support windows
    fixes #2

---
+++ process.go
@@ -0,0 +1,41 @@
+package goof
+
+import (
+	"debug/dwarf"
+	"debug/elf"
+	"debug/macho"
+	"debug/pe"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+)
+
+func openProc() (*dwarf.Data, error) {
+	path, err := os.Executable()
+	if err != nil {
+		return nil, err
+	}
+
+	var fh interface {
+		io.Closer
+		DWARF() (*dwarf.Data, error)
+	}
+
+	switch runtime.GOOS {
+	case "linux":
+		fh, err = elf.Open(path)
+	case "darwin":
+		fh, err = macho.Open(path)
+	case "windows":
+		fh, err = pe.Open(path)
+	default:
+		return nil, fmt.Errorf("unknown goos: %q", runtime.GOOS)
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer fh.Close()
+
+	return fh.DWARF()
+}
